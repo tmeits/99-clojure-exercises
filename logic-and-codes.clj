871a31fa7248ed167fb5a17194aaee2a78461842
tmeits <tmeits@gmail.com>
2011-07-14 17:04:13 +0800

    add test function

--- logic-and-codes.clj
+++ logic-and-codes.clj
@@ -3,9 +3,23 @@
   (:use clojure.contrib.profile)
   (:use clojure.test))
 
+(deftest test-and-or-nor-nand-xor-impl-equ
+  "Test codee for remaining logic functions"
+  (do
+    (is (= (not1 true) false))
+    (is (= (not1 false) true))
+    (is (= (and2 true true) true))
+    (is (= (and2 true false) false))
+    (is (= (and2 false true) false))
+    (is (= (and2 false false) false))
+    ))
+
 (defn and2 [a b]
   "AND is True if both a and b are True"
   (if a (if b true false) false))
+(defn not1 [a]
+  "NOT negates a single Boolean argument"
+  (if a false true))
 
 (comment
   http://www.haskell.org/haskellwiki/99_questions/Solutions/46

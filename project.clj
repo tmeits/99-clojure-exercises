6a51ebba9a8920a21fea81991d2bcf015e7f5443
tmeits <tmeits@gmail.com>
2011-07-14 13:33:20 +0800

    Has added the first symbol in the project name, there was an error lein

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject 99-clojure-exercises "0.1-SNAPSHOT"
+(defproject c99-clojure-exercises "0.1-SNAPSHOT"
   :description "C-99: Ninety-Nine Clojure Problems"
   :dependencies [[org.clojure/clojure "1.2.1"]])

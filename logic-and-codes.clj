766e5941d04381cfb0ee57745ae0ea62b5b0c5ee
tmeits <tmeits@gmail.com>
2011-07-18 13:30:36 +0800

    add Test codee for remaining logic functions

--- logic-and-codes.clj
+++ logic-and-codes.clj
@@ -16,7 +16,27 @@
        (c99-or true true) true
        (c99-or false false) false
        (c99-or true false) true
-       (c99-or false true) true)
+       (c99-or false true) true
+       (c99-nor true true) false
+       (c99-nor false false) true
+       (c99-nor true false) false
+       (c99-nor false true) false
+       (c99-nand true true) false
+       (c99-nand true false) true
+       (c99-nand false true) true
+       (c99-nand false false) true
+       (c99-xor true true) false
+       (c99-xor false false) false
+       (c99-xor true false) true
+       (c99-xor false true) true
+       (c99-impl true true) true
+       (c99-impl false false) true
+       (c99-impl false true) true
+       (c99-impl true false) false
+       (c99-equ true true) true
+       (c99-equ false false) true
+       (c99-equ false true) false
+       (c99-equ true false) false
        ))
 
 (defn c99-and [a b]
@@ -28,7 +48,28 @@
 (defn c99-or [a b]
   "OR is True if a or b or both are True"
   (if a true (if b true false)))
+(defn c99-nor [a b]
+  "NOR is the negation of 'or'"
+  (c99-not (c99-or a b)))
+(defn c99-nand [a b]
+  "NAND is the negation of 'and'"
+  (c99-not (c99-and a b)))
+(defn c99-xor [a b]
+  "XOR is True if either a or b is True, but not if both are True"
+  (cond
+   (c99-and (= a true) (= b false)) true
+   (c99-and (= a false) (= b true)) true
+   :else false))
+(defn c99-impl [a b]
+  "IMPL is True if a implies b, equivalent to (not a) or (b)"
+  (c99-or (c99-not a) b))
+(defn c99-equ [a b]
+  "EQU is True if a and b are equal"
+  (= a b))
+
 (comment
   http://www.haskell.org/haskellwiki/99_questions/Solutions/46
   http://www.clojure-toolbox.com/
   )
+
+(run-tests)

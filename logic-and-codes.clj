96a9cb41f1b14df37ef4cd2d99d5586131f10ff8
tmeits <tmeits@gmail.com>
2011-07-20 13:33:55 +0800

    Now, here's one for run-time: c99-truth-tables-two

--- logic-and-codes.clj
+++ logic-and-codes.clj
@@ -49,28 +49,24 @@
   Define a function that takes a logical expression (a function of two variables)
   and prints the truth table."
   (cond
-    (= a b) '()
-    :else 
-    (for [x '(true false) y '(true false)]
-    ((fn [p1 p2] (def a p1) (def b p2)  (list p1 p2 (eval sexp))) x y))
-    ))
-
-(defmacro defn-with-str [string args & body]
- `(defn ~(symbol string) ~args ~@body))
-
-(defn-with-str "print-string" [k] (println k))
-
-(print-string "lol")
-
+   (= a b) '()
+   :else 
+   (for [i '(true false) j '(true false)]
+     ((fn [p1 p2] (intern *ns* (symbol (str a)) p1) (intern *ns* (symbol (str b)) p2)
+        (list p1 p2 (eval sexp))) i j))))
 
 
 (deftest table-test "Truth tables for logical expressions."
   (is (= (c99-truth-tables 'A 'B '(and A (or A B)))
          '((true true true) (true false true) (false true false) (false false false)))
       "")
-  (is (= (c99-truth-tables-two 'D 'F '(and D (or D F)))
+  (is (= (c99-truth-tables-two 'D 'D '(and D (or D F)))
+         '())
+      "")
+  (is (= (c99-truth-tables-two 'A 'B '(and A (or A B)))
          '((true true true) (true false true) (false true false) (false false false)))
-      ""))
+    "")
+  )
 
 (deftest logic-and-code "Test codee for remaining logic functions"
   (are [x y] (= x y)
